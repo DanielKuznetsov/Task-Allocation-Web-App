d96dc5fb259caeade2ab52450ad32182537aba62
Daniel Kuznetsov <danilgkuznet@icloud.com>
2023-12-09 14:08:58 -0800

    Added vercel analytics

--- ta-app/front-end/my-app/pages/_app.js
+++ ta-app/front-end/my-app/pages/_app.js
@@ -4,6 +4,7 @@ import store from "../app/store";
 import { Provider } from "react-redux";
 import { MantineProvider } from "@mantine/core";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }) {
       <Provider store={store}>
         <Component {...pageProps} />
         <SpeedInsights />
+        <Analytics />
       </Provider>
     </MantineProvider>
   );
