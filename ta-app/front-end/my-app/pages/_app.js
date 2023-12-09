731df531a5de88cc94773ace4c9c45704b7cfaf3
Daniel Kuznetsov <danilgkuznet@icloud.com>
2023-12-09 14:04:08 -0800

    Added Speed Insights from Vercel

--- ta-app/front-end/my-app/pages/_app.js
+++ ta-app/front-end/my-app/pages/_app.js
@@ -3,12 +3,14 @@ import "@mantine/core/styles.css";
 import store from "../app/store";
 import { Provider } from "react-redux";
 import { MantineProvider } from "@mantine/core";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }) {
   return (
     <MantineProvider>
       <Provider store={store}>
         <Component {...pageProps} />
+        <SpeedInsights />
       </Provider>
     </MantineProvider>
   );
